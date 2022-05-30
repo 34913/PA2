db0ead16a60947af5eda7aced7248758cfabda36
František Holý <holyfra1@fit.cvut.cz>
2022-05-31 00:28:40 +0200

    AntWars: Basic work on map saving and interacting, need more work done on that

---
+++ AntWars/Map.cpp
@@ -0,0 +1,69 @@
+#include "Map.h"
+
+Map::Map()
+{}
+
+std::ostream& operator<<(std::ostream& os, Map& obj)
+{
+	os << obj.GetWidth() << " " << obj.GetHeight() << std::endl;
+	for (int i = 0; i < obj.GetHeight(); i++) {
+		for (int w = 0; w < obj.GetWidth(); w++) {
+
+			os.put(obj.arr[i][w]);
+		}
+		os << std::endl;
+	}
+
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Map& obj)
+{
+	int width, height;
+	if (!(is >> width >> height))
+		throw std::invalid_argument("Cant read size of playing field, corrupted");
+
+	if (width <= 0 || height <= 0)
+		throw std::invalid_argument("File corrupted, wrong size of map");
+
+	char ch = is.get();
+	obj.arr.clear();
+	for (int i = 0; i < height; i++) {
+		obj.arr.push_back({});
+		for (int w = 0; w < width; w++) {
+
+			if (!is.good())
+				throw std::invalid_argument("File corrupted, not enough characters in map");
+			ch = is.get();
+			obj.arr[i].push_back(ch);
+		}
+		if ((ch = is.get()) != '\n' || !is.good())
+			throw std::invalid_argument("File corrupted, wrong line ending");
+	}
+
+	return is;
+}
+
+std::vector<char>& Map::operator[](size_t x)
+{
+	if (x < 0 || x >= arr.size())
+		throw std::invalid_argument("Out of bounds");
+	return arr[x];
+}
+
+size_t Map::GetHeight() const
+{
+	return arr.size();
+}
+
+size_t Map::GetWidth() const
+{
+	if (GetHeight() == 0)
+		return 0;
+	return arr[0].size();
+}
+
+std::vector<std::vector<char>> Map::GetArr() const
+{
+	return arr;
+}
---
+++ AntWars/Map.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <vector>
+#include <ostream>
+#include <fstream>
+
+class Map
+{
+	// playing field
+	std::vector<std::vector<char>> arr;
+
+public:
+
+	// accesible constants
+
+	static const char EMPTY = ' ';
+	static const char WALL = '#';
+
+	// cons/destructor
+
+	Map();
+
+
+	// operators
+
+
+	friend std::ostream& operator << (std::ostream& os, Map& obj);
+
+	friend std::istream& operator >> (std::istream& is, Map& obj);
+
+	std::vector<char>& operator [] (size_t x);
+
+	// methods
+
+
+
+	// getters
+
+	/**
+	 * Height getter.
+	 * 
+	 * \return height of playing field
+	 */
+	size_t GetHeight() const;
+
+	/**
+	 * Width getter.
+	 * 
+	 * \return width of playing field
+	 */
+	size_t GetWidth() const;
+
+	/**
+	 * Whole map getter.
+	 * 
+	 * \return map with its contents
+	 */
+	std::vector<std::vector<char>> GetArr() const;
+
+};
