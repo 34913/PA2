3ac2171ed4087d0b2b1512d78d794991b699de2d
František Holý <holyfra1@fit.cvut.cz>
2022-06-09 02:03:51 +0200

    AntWars: Some code to make AutoPlayer somehow decide which troops need to be trained in the moment

--- AntWars/AutoPlayer.cpp
+++ AntWars/AutoPlayer.cpp
@@ -4,9 +4,54 @@ AutoPlayer::AutoPlayer(const std::string& name)
 	:Player(name)
 {}
 
+void AutoPlayer::SetLimits(int melee, int ranged, int tank)
+{
+	tankLimit = tank;
+	rangedLimit = ranged;
+	meleeLimit = melee;
+}
+
 void AutoPlayer::Input()
 {
+	auto now = std::chrono::steady_clock::now();
+	std::chrono::milliseconds millis;
 
+	for (auto& x : bases) {
 
+		if (!x.second.train.empty())
+			continue;
 
+		selectedBase = x.first;
+
+		if (golds.GetMoney() >= costs[TankAnt::type]) {
+			millis = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastTank);
+
+			if (millis.count() > tankLimit) {
+				Player::Input(Command::trainTank);
+				lastTank = now;
+			
+				continue;
+			}
+		}
+		if (golds.GetMoney() >= costs[RangedAnt::type]) {
+			millis = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastRanged);
+
+			if (millis.count() > rangedLimit) {
+				Player::Input(Command::trainTank);
+				lastRanged = now;
+
+				continue;
+			}
+		}
+		if (golds.GetMoney() >= costs[MeleeAnt::type]) {
+			millis = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMelee);
+
+			if (millis.count() > meleeLimit) {
+				Player::Input(Command::trainTank);
+				lastMelee = now;
+
+				continue;
+			}
+		}
+	}
 }
--- AntWars/AutoPlayer.h
+++ AntWars/AutoPlayer.h
@@ -5,9 +5,26 @@
 
 class AutoPlayer: public Player
 {
+	int tankLimit = 10000;
+	int rangedLimit = 5000;
+	int meleeLimit = 500;
+
+	std::chrono::steady_clock::time_point lastTank = std::chrono::steady_clock::now();
+	std::chrono::steady_clock::time_point lastRanged = std::chrono::steady_clock::now();
+	std::chrono::steady_clock::time_point lastMelee = std::chrono::steady_clock::now();
+
 public:
 	
 	AutoPlayer(const std::string& name = "Automat");
+
+	/**
+	 * Sets the training time limits in milliseconds.
+	 * 
+	 * \param melee melee limit
+	 * \param ranged ranged limit
+	 * \param tank tank limit
+	 */
+	void SetLimits(int melee, int ranged, int tank);
 
 	void Input();
 
